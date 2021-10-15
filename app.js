7ef4fd83e6598f260e6165320fb759d0ebeec53b
Gabriel <52950086+Gabriel737@users.noreply.github.com>
2021-10-14 23:40:46 -0700

    Completed part 4B

--- app.js
+++ app.js
@@ -27,4 +27,22 @@ app.post('/api/blogs', async (request, response) => {
   response.status(201).json(result)
 })
 
+app.put('/api/blogs/:id', async (request, response) => {
+  const body = request.body
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.status(201).json(updatedBlog)
+})
+
+app.delete('/api/blogs/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
 export default app
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -29,6 +29,113 @@ describe('General Tests', () => {
       .expect(200)
       .expect('Content-Type', /application\/json/)
   })
+
+  test('Blogs have ID property', async () => {
+    const response = await api.get('/api/blogs')
+    for(let blog of response.body) {
+      expect(blog.id).toBeDefined()
+    }
+  })
+})
+
+describe('Addition of a blog', () => {
+
+  test('Added blog gets added to DB', async () => {
+
+    const oldBlogs = await helper.getBlogs()
+
+    const newBlog = {
+      'title': 'Test',
+      'author': 'Test',
+      'url': 'Test',
+      'likes': 0
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.getBlogs()
+    expect(blogsAtEnd).toHaveLength(oldBlogs.length + 1)
+  })
+
+  test('Added blog without like value gets defaulted to 0', async () => {
+    const newBlog = {
+      'title': 'Test',
+      'author': 'Test',
+      'url': 'Test',
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    expect(response.body.likes).toBe(0)
+  })
+
+  test('Added blog without title gets rejected', async () => {
+    const newBlog = {
+      'author': 'Test',
+      'url': 'Test',
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(500)
+  })
+
+  test('Added blog without url gets rejected', async () => {
+    const newBlog = {
+      'title': 'Test',
+      'author': 'Test',
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(500)
+  })
+})
+
+describe('Updating of a blog', () => {
+
+  test('Updating of first blog updates it in DB', async () => {
+    const oldBlogs = await helper.getBlogs()
+    let oldBlog = oldBlogs[0]
+    const updatedBlog = {
+      title: oldBlog.title,
+      author: oldBlog.author,
+      url: oldBlog.url,
+      likes: oldBlog.likes + 1
+    }
+
+    await api
+      .put(`/api/blogs/${oldBlog.id}`)
+      .send(updatedBlog)
+      .expect(201)
+
+    const result = await Blog.findById(oldBlog.id)
+    expect(result.likes).toBe(oldBlog.likes + 1)
+  })
+})
+
+describe('Deletion of a blog', () => {
+  test('Deletion of first blog removes it from DB', async () => {
+    const oldBlogs = await helper.getBlogs()
+
+    await api
+      .delete(`/api/blogs/${oldBlogs[0].id}`)
+      .expect(204)
+
+    const newBlogs = await helper.getBlogs()
+
+    expect(newBlogs).toHaveLength(oldBlogs.length - 1)
+  })
 })
 
 afterAll(() => {
